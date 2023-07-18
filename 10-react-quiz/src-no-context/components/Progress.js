c9f5eead470fd3a4fad252cac4f53cd84488b26c
Ahmed Ayman <ahmed1997164@gmail.com>
2023-07-18 22:38:54 +0300

    react-quiz move old version to src-no-context
    Took 10 minutes

---
+++ 10-react-quiz/src-no-context/components/Progress.js
@@ -0,0 +1,9 @@
+export const Progress = ({index, numQuestions, points, maxPossiblePoints,answer}) => {
+    return (
+        <header className="progress">
+            <progress max={numQuestions} value={index+Number(answer!==null)}></progress>
+            <p>Question <strong>{index}</strong> / <strong>{numQuestions}</strong></p>
+            <p><strong>{points}</strong> / <strong>{maxPossiblePoints}</strong></p>
+        </header>
+    );
+};
