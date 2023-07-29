0fc196e8412f2e499bca8287a771462b1ab62685
Ahmed Ayman <ahmed1997164@gmail.com>
2023-07-29 14:45:03 +0300

    Adding supabase

---
+++ 17-the-wild-oasis/src/services/supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "../../types/supabase";
+
+const supabaseUrl: string = "https://chnnozetrcnmyuvhjyql.supabase.co";
+const supabaseKey: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNobm5vemV0cmNubXl1dmhqeXFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA2MjQyNzEsImV4cCI6MjAwNjIwMDI3MX0.VNkx8i9Ar2VDLQm8xCQAvLyCnaF4OwPhfjksozqed_0";
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+
+export default supabase;
